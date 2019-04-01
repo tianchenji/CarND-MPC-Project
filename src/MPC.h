cf3064006ef451f5a2fc1f2327a7424213e35e97
tianchenji <48822343+tianchenji@users.noreply.github.com>
2019-04-01 20:18:44 +0800

    Add files via upload

--- src/MPC.h
+++ src/MPC.h
@@ -4,6 +4,8 @@
 #include <vector>
 #include "Eigen-3.3/Eigen/Core"
 
+using namespace std;
+
 class MPC {
  public:
   MPC();
@@ -11,9 +13,8 @@ class MPC {
   virtual ~MPC();
 
   // Solve the model given an initial state and polynomial coefficients.
-  // Return the first actuations.
-  std::vector<double> Solve(const Eigen::VectorXd &state, 
-                            const Eigen::VectorXd &coeffs);
+  // Return the first actuatotions.
+  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
 };
 
-#endif  // MPC_H
+#endif /* MPC_H */
